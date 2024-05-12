c716677c288cfd121c688bd8bf07e6c4ec07e44c
Ange Curé <ange.cure@alumni.univ-avignon.fr>
2024-05-12 15:54:19 +0200

    Add: implement start music and pause rtsp flux Changes: change button to start speech recognition

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zeroc.Ice.Communicator;
 import com.zeroc.Ice.Util;
 
 import MusicServer.MusicManagerPrx;
+import MusicServer.MusicNotFoundError;
 
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -21,6 +24,7 @@ import android.speech.SpeechRecognizer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -34,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
     ImageButton buttonPrevious;
     ImageButton buttonNext;
     ImageButton buttonPlay;
+    MusicManagerPrx musicManager;
+    MediaPlayer mediaPlayer;
+    FloatingActionButton buttonMircro;
 
     private SpeechRecognizer speechRecognizer;
 
@@ -50,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
         buttonPrevious = this.findViewById(R.id.buttonPrevious);
         buttonNext = this.findViewById(R.id.buttonNext);
         buttonPlay = this.findViewById(R.id.buttonPlay);
+        buttonMircro = this.findViewById(R.id.buttonMircro);
 
         buttonPrevious.setOnClickListener(v -> {
             if (lastPlayed > 0) {
@@ -66,6 +74,10 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
         });
 
         buttonPlay.setOnClickListener(v -> {
+            musicManager.pauseMusic();
+        });
+
+        buttonMircro.setOnClickListener(v -> {
             if (speechRecognizer != null) {
                 // Créez un Intent pour la reconnaissance vocale
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -81,14 +93,10 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
 
         String[] customArgs = new String[]{"--Ice.MessageSizeMax=0"};
         Communicator communicator = Util.initialize(customArgs);
-        MusicManagerPrx musicManager = MusicManagerPrx.checkedCast(communicator.stringToProxy("MusicManager:default -h 192.168.0.19 -p 10000"));
+        musicManager = MusicManagerPrx.checkedCast(communicator.stringToProxy("MusicManager:default -h 192.168.0.19 -p 10000"));
         MusicServer.MusicInfo[] musicInfos = musicManager.listAllMusic();
 
         List<Song> songs = new ArrayList<>();
-        for (MusicServer.MusicInfo music: musicInfos) {
-            songs.add(new Song(music.title, music.artist));
-
-        }
         for (MusicServer.MusicInfo music: musicInfos) {
             songs.add(new Song(music.title, music.artist));
 
@@ -98,6 +106,10 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
         adapter.setOnTitleClickListener(this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+        mediaPlayer = new MediaPlayer();
+
+
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         speechRecognizer.setRecognitionListener(new RecognitionListener() {
@@ -168,6 +180,17 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
         adapter.notifyItemChanged(position);
         lastPlayed = position;
         adapter.getItem(0);
+        try {
+            musicManager.playMusic(song.getTitle(), song.getArtist());
+//            mediaPlayer.reset();
+//            mediaPlayer.setDataSource("rtsp://192.168.0.19:554/stream"); // Replace with your RTSP URL
+//            mediaPlayer.prepare();
+//            mediaPlayer.start();
+        } catch (MusicServer.MusicNotFoundError e) {
+            System.out.println("La musique n'a pas été trouvée.");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -176,5 +199,6 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnTit
         if (speechRecognizer != null) {
             speechRecognizer.destroy();
         }
+        mediaPlayer.release();
     }
 }
